6da725fa82f05366dd831f06d43686ab950b219c
Michael J. Simons <michael@simons.ac>
2014-02-08 17:06:12 +0100

    Worked on the bike

--- src/main/java/ac/simons/biking2/persistence/entities/Bike.java
+++ src/main/java/ac/simons/biking2/persistence/entities/Bike.java
@@ -13,13 +13,58 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ac.simons.biking2.persistence.entities;
 
+import java.io.Serializable;
+import java.time.Instant;
+import static java.time.Instant.now;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotBlank;
+
 /**
- *
  * @author Michael J. Simons, 2014-02-08
  */
-public class Bike {
-    
+@Entity
+@Table(name = "bikes")
+public class Bike implements Serializable {
+
+    private static final long serialVersionUID = 1249824815158908981L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", unique = true, length = 255, nullable = false)
+    @NotBlank
+    @Size(max = 255)
+    private String name;
+
+    @Column(name = "color", length = 6, nullable = false)
+    @NotBlank
+    @Size(max = 6)
+    private String color = "CCCCCC";
+
+    @Column(name = "created_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
+    private Date createdAt;
+
+    @PrePersist
+    public void prePersist() {
+	if (this.createdAt == null) {
+	    this.createdAt = Date.from(now());
+	}
+    }
 }
