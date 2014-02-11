d58854c4ca664ab37a58b5a06791a1711c4c0184
Michael J. Simons <michael@simons.ac>
2014-02-11 17:43:52 +0100

    Added representation of charts object

---
+++ src/main/java/ac/simons/biking2/highcharts/Chart.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2014 Michael J. Simons.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ac.simons.biking2.highcharts;
+
+import ac.simons.biking2.misc.Sink;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+/**
+ * @author Michael J. Simons, 2014-02-11
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Chart {
+
+    public static class Builder<PB> {
+
+	private final Sink<PB, Chart> sink;
+
+	private Integer borderWidth;
+
+	private String type;
+
+	public Builder(final Sink<PB, Chart> sink) {
+	    this.sink = sink;
+	}
+
+	public Builder withBorderWidth(final Integer borderWidth) {
+	    this.borderWidth = borderWidth;
+	    return this;
+	}
+
+	public Builder withType(final String type) {
+	    this.type = type;
+	    return this;
+	}
+
+	public PB build() {
+	    return this.sink.setObject(
+		    new Chart(borderWidth, type)
+	    );
+	}
+    }
+
+    private final Integer borderWidth;
+
+    private final String type;
+
+    public Chart(Integer borderWidth, String type) {
+	this.borderWidth = borderWidth;
+	this.type = type;
+    }
+
+    public Integer getBorderWidth() {
+	return borderWidth;
+    }
+
+    public String getType() {
+	return type;
+    }
+}
---
+++ src/test/java/ac/simons/biking2/highcharts/ChartTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2014 Michael J. Simons.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ac.simons.biking2.highcharts;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+/**
+ * @author Michael J. Simons, 2014-02-11
+ */
+public class ChartTest {
+
+    @Test
+    public void testBuilder() {
+	new Chart.Builder<>(chart -> {
+	    assertThat(chart.getBorderWidth(), is(nullValue()));
+	    assertThat(chart.getType(), is(nullValue()));
+	    return ChartTest.this;
+	})
+	.build();
+
+	new Chart.Builder<>(chart -> {
+	    assertThat(chart.getBorderWidth(), is(equalTo(1)));
+	    assertThat(chart.getType(), is(equalTo("line")));
+	    return ChartTest.this;
+	})
+		.withBorderWidth(1)
+		.withType("line")
+	.build();
+    }
+}
