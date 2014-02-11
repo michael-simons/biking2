15a7b187c31ffbeae7b37723df5a5b9f726906a9
Michael J. Simons <michael@simons.ac>
2014-02-11 22:17:35 +0100

    Added plot options

---
+++ src/test/java/ac/simons/biking2/highcharts/PlotOptionsTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 Michael J. Simons.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ac.simons.biking2.highcharts;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+/**
+ * @author Michael J. Simons, 2014-02-11
+ */
+public class PlotOptionsTest {
+    
+    @Test
+    public void testBuilder() {
+	PlotOptions plotOptions = new PlotOptions.Builder<>(object -> object)
+		.column()		    
+		    .withPointPadding(0.2)
+		    .withBorderWidth(0)
+		.build()
+		.series()
+		    .disableAnimation()
+		.build()	
+	.build();
+	assertThat(plotOptions.getColumn().getPointPadding(), is(equalTo(0.2)));
+	assertThat(plotOptions.getColumn().getBorderWidth(), is(equalTo(0)));
+	assertThat(plotOptions.getSeries().isAnimation(), is(false));
+    }
+    
+}
