bee9cb0b65e7e950e4b889f511dd23bcedd025a6
Michael J. Simons <michael@simons.ac>
2014-02-12 22:33:02 +0100

    fixed non visible types

--- src/main/java/ac/simons/biking2/highcharts/SeriesOptions.java
+++ src/main/java/ac/simons/biking2/highcharts/SeriesOptions.java
@@ -30,7 +30,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SeriesOptions {
 
-    static class Builder<PB> {
+    public static class Builder<PB> {
 
 	private final Sink<PB, SeriesOptions> sink;
 
