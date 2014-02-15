2cfd8d6bc4c5afc97dbcc3c79bd5aa871c895e5b
Michael J. Simons <michael@simons.ac>
2014-02-15 19:53:31 +0100

    reconfigured datasource config

--- src/main/java/ac/simons/biking2/config/DataStorageConfig.java
+++ src/main/java/ac/simons/biking2/config/DataStorageConfig.java
@@ -13,32 +13,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package ac.simons.biking2.config;
 
 import java.io.File;
-import javax.sql.DataSource;
-import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
 /**
- * @author Michael J. Simons, 2014-02-08
+ * @author Michael J. Simons, 2014-02-15
  */
 @Configuration
 @Profile({"dev", "prod"})
-public class DataSourceConfig {
-
+public class DataStorageConfig {
     @Bean
-    public DataSource dataSource(final File datastoreBaseDirectory, final Environment environment) {
-	final JdbcDataSource rv = new JdbcDataSource();
-
-	final String defaultDatabaseFile = new File(datastoreBaseDirectory, String.format("db/biking-%s", environment.acceptsProfiles("dev") ? "dev" : "prod")).getAbsolutePath();
-	rv.setUrl(String.format("jdbc:h2:file:%s;FILE_LOCK=FS", environment.getProperty("biking2.database-file", defaultDatabaseFile)));
-	rv.setUser("biking2");
-	rv.setPassword("biking2");
-
+    public File datastoreBaseDirectory(final Environment environment) {
+	final String customDatastoreBaseDirectory = environment.getProperty("biking2.datastore-base-directory");
+	File rv;
+	if(customDatastoreBaseDirectory == null || customDatastoreBaseDirectory.isEmpty())
+	    rv = new File(System.getProperty("user.dir"), String.format("var/%s", environment.acceptsProfiles("dev") ? "dev" : "prod"));
+	else
+	    rv = new File(customDatastoreBaseDirectory);
+	
+	if(!(rv.isDirectory() || rv.mkdir()))
+	    throw new RuntimeException(String.format("Could not initialize '%s' as base directory for datastore!", rv.getAbsolutePath()));
+	
+	new File(rv, "data/bikingPictures").mkdirs();
+	new File(rv, "data/galleryPictures").mkdirs();
+	new File(rv, "data/tracks").mkdirs();
+		
 	return rv;
-    }
+    } 
 }
