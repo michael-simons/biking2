0ae0da84454ed40c1fb325f96069d71119c79e65
Michael J. Simons <michael@simons.ac>
2014-02-15 17:11:49 +0100

    added test for getTracks()

--- src/main/java/ac/simons/biking2/api/TracksController.java
+++ src/main/java/ac/simons/biking2/api/TracksController.java
@@ -20,7 +20,6 @@ import ac.simons.biking2.persistence.entities.Track;
 import ac.simons.biking2.persistence.repositories.TrackRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
