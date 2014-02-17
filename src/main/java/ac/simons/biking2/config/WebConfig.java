642a9edf893314fe7adbdb3d33b6b018da428798
Michael J. Simons <michael@simons.ac>
2014-02-17 14:16:28 +0100

    configure format parameter

--- src/main/java/ac/simons/biking2/config/WebConfig.java
+++ src/main/java/ac/simons/biking2/config/WebConfig.java
@@ -20,13 +20,26 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 /**
  * @author Michael J. Simons, 2014-02-15
  */
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * Enable favor of format parameter over requested content type, needed
+     * for #OEmbedController#getEmbeddableTrack
+     * @param configurer 
+     */
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	super.configureContentNegotiation(configurer); 
+	configurer.favorParameter(true);
+    }
 
     /**
      * This makes mapping of
