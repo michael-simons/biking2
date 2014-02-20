59b91de7258af8874918e9ea9bf62bad769c5890
Michael J. Simons <michael@simons.ac>
2014-02-20 12:07:57 +0100

    use default in memory config

--- src/main/java/ac/simons/biking2/config/SecurityConfig.java
+++ src/main/java/ac/simons/biking2/config/SecurityConfig.java
@@ -15,17 +15,14 @@
  */
 package ac.simons.biking2.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- *
  * @author Michael J. Simons, 2014-02-19
  */
 @Configuration
@@ -36,17 +33,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-	http.
-	    httpBasic()
-		.and()
-	    .csrf().disable();
+	http
+	    .httpBasic()
+	    .and()
+	    .csrf()
+		.disable();
     }
-
-    @Autowired
-    public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
-	auth
-		.inMemoryAuthentication()
-		.withUser("user").password("password").roles("USER");
-    }
-
 }
