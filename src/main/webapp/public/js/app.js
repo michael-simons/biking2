766004b4a998e0b70975c63a2ceb9874c9ce4dc7
Michael J. Simons <michael@simons.ac>
2014-02-23 08:57:50 +0100

    fixed endles loop on empty array

--- src/main/webapp/public/js/app.js
+++ src/main/webapp/public/js/app.js
@@ -20,6 +20,8 @@
 
 Array.prototype.randomize = function() {
     var i = this.length, j, temp;
+    if(i==0)
+	return;
     while (--i) {
 	j = Math.floor(Math.random() * (i - 1));
 	temp = this[i];
