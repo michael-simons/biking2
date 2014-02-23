271dd8b3dfd54672d849d9e3d9dc75ce50f17ef2
Michael J. Simons <michael@simons.ac>
2014-02-23 08:59:41 +0100

    really fixed the endless loop

--- src/main/webapp/public/js/app.js
+++ src/main/webapp/public/js/app.js
@@ -20,8 +20,8 @@
 
 Array.prototype.randomize = function() {
     var i = this.length, j, temp;
-    if(i==0)
-	return;
+    if(i===0)
+	return this;
     while (--i) {
 	j = Math.floor(Math.random() * (i - 1));
 	temp = this[i];
