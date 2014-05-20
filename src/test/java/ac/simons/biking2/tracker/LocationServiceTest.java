2f73ad4cff0d992e2db221f8a1d10aa9c1ac858e
Michael J. Simons <michael@simons.ac>
2014-05-20 16:24:07 +0200

    Finished tests for location service

--- src/test/java/ac/simons/biking2/tracker/LocationServiceTest.java
+++ src/test/java/ac/simons/biking2/tracker/LocationServiceTest.java
@@ -17,6 +17,9 @@ package ac.simons.biking2.tracker;
 
 import ac.simons.biking2.persistence.entities.Location;
 import ac.simons.biking2.persistence.repositories.LocationRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -25,12 +28,14 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.AdditionalAnswers;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.stub;
 
@@ -38,6 +43,8 @@ import static org.mockito.Mockito.stub;
  * @author Michael J. Simons, 2014-05-20
  */
 public class LocationServiceTest {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    
     @Test
     public void shouldGetLocationsForTheLastNHours() {
 	final LocationRepository locationRepository = Mockito.mock(LocationRepository.class);	
@@ -54,5 +61,27 @@ public class LocationServiceTest {
 	Mockito.verify(locationRepository).findByCreatedAtGreaterThanOrderByCreatedAtAsc(argument.capture());	  
 	
 	Assert.assertThat(ChronoUnit.HOURS.between(argument.getValue().toInstant(), Instant.now()), is(equalTo(3l)));    
+    }
+    
+    @Test
+    public void shouldCreateAndSendNewLocation() throws IOException {
+	final LocationRepository locationRepository = Mockito.mock(LocationRepository.class);	
+	stub(locationRepository.save(any(Location.class))).toAnswer(AdditionalAnswers.returnsFirstArg());
+	final SimpMessagingTemplate simpMessagingTemplate = mock(SimpMessagingTemplate.class);
+	
+	final LocationService locationService = new LocationService(locationRepository, simpMessagingTemplate);
+	
+	final Location location = locationService.createAndSendNewLocation(objectMapper.readValue("{\"lon\":\"5\", \"lat\":\"50\"}", NewLocationCmd.class));
+	Assert.assertEquals(new BigDecimal(50), location.getLatitude());
+	Assert.assertEquals(new BigDecimal(5), location.getLongitude());
+	
+	Mockito.verify(locationRepository).save(any(Location.class));
+	final ArgumentCaptor<String> destinationNameArg = ArgumentCaptor.forClass(String.class);
+	final ArgumentCaptor<Location> locationArg = ArgumentCaptor.forClass(Location.class);
+	Mockito.verify(simpMessagingTemplate).convertAndSend(destinationNameArg.capture(), locationArg.capture());	  
+	Mockito.verifyNoMoreInteractions(locationRepository, simpMessagingTemplate);
+	
+	Assert.assertEquals("/topic/currentLocation", destinationNameArg.getValue());
+	Assert.assertEquals(location, locationArg.getValue());
     }
 }
