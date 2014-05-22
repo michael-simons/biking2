b2839ed3ade6f14457901c533b37b1a24acb7450
Michael J. Simons <michael@simons.ac>
2014-05-22 12:57:59 +0200

    Implemented missing tests

--- src/test/java/ac/simons/biking2/jobs/FetchBikingPicturesJobTest.java
+++ src/test/java/ac/simons/biking2/jobs/FetchBikingPicturesJobTest.java
@@ -21,17 +21,24 @@ import ac.simons.biking2.persistence.repositories.BikingPictureRepository;
 import ac.simons.biking2.rss.RSSDateTimeAdapter;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URLConnection;
 import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.GregorianCalendar;
 import java.util.List;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
 
 import static java.time.ZonedDateTime.of;
+import static java.util.Calendar.getInstance;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.rules.ExpectedException.none;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.stub;
@@ -43,6 +50,8 @@ import static org.mockito.Mockito.verify;
  * @author Michael J. Simons
  */
 public class FetchBikingPicturesJobTest {
+    @Rule
+    public final ExpectedException expectedException = none();
 
     private final RSSDateTimeAdapter dateTimeAdapter = new RSSDateTimeAdapter();
     private final File tmpDir;
@@ -85,7 +94,34 @@ public class FetchBikingPicturesJobTest {
 	verify(dailyFratzeProvider).getRSSConnection(null);
 	verify(dailyFratzeProvider).getRSSConnection(page2);
     }
+    
+    @Test
+    public void jobShouldHandleExceptionsGracefully() throws MalformedURLException, IOException, Exception {
+	final DailyFratzeProvider dailyFratzeProvider = mock(DailyFratzeProvider.class);
+	stub(dailyFratzeProvider.getRSSConnection(null)).toReturn(new File("/i/dont/exists").toURI().toURL().openConnection());
+	
+	final BikingPictureRepository bikingPictureRepository = mock(BikingPictureRepository.class);
+	stub(bikingPictureRepository.getMaxPubDate()).toReturn(GregorianCalendar.from(dateTimeAdapter.unmarshal("Sun, 08 May 2011 18:38:25 GMT")));	
+	
+	final FetchBikingPicturesJob job = new FetchBikingPicturesJob(dailyFratzeProvider, bikingPictureRepository, tmpDir);
+	final List<BikingPicture> toDownload = job.createDownloadList();
+	assertThat(toDownload.size(), is(equalTo(0)));
+    }
 
+    @Test
+    public void jobShouldThrowExceptionOnInvalidStorageDir() throws IOException {
+	final DailyFratzeProvider dailyFratzeProvider = mock(DailyFratzeProvider.class);	
+	final BikingPictureRepository bikingPictureRepository = mock(BikingPictureRepository.class);
+	
+	final File someFile = new File(System.getProperty("java.io.tmpdir"), "deleteMeReally");
+	someFile.deleteOnExit();
+	Assert.assertTrue(someFile.createNewFile());
+	
+	expectedException.expect(RuntimeException.class);
+	expectedException.expectMessage("Could not create bikingPicturesStorage!");
+	new FetchBikingPicturesJob(dailyFratzeProvider, bikingPictureRepository, someFile);
+    }
+    
     @Test
     public void jobShouldAbortWhenNoMorePagesAreAvailable() throws Exception {
 	final DailyFratzeProvider dailyFratzeProvider = mock(DailyFratzeProvider.class);
@@ -141,5 +177,23 @@ public class FetchBikingPicturesJobTest {
 	assertThat(new File(this.tmpDir, PersistenceConfig.BIKING_PICTURES_DIRECTORY + "/43461.jpg").isFile(), is(false));
 	// Verify number of calls with arbitrary ints
 	verify(dailyFratzeProvider, times(3)).getImageConnection(anyInt());
+    }
+    
+    @Test
+    public void runMethodShouldWorkAsExpected() throws IOException {
+	final DailyFratzeProvider dailyFratzeProvider = mock(DailyFratzeProvider.class);
+	stub(dailyFratzeProvider.getRSSConnection(null)).toReturn(this.getClass().getResource("/biking_pictures.rss").openConnection());
+	
+	final BikingPictureRepository bikingPictureRepository = mock(BikingPictureRepository.class);	
+	stub(bikingPictureRepository.getMaxPubDate()).toReturn(getInstance());	
+	
+	final FetchBikingPicturesJob job = new FetchBikingPicturesJob(dailyFratzeProvider, bikingPictureRepository, tmpDir);
+	job.run();
+	
+	verify(dailyFratzeProvider).getRSSConnection(null);
+	verify(bikingPictureRepository).getMaxPubDate();
+	
+	Mockito.verifyNoMoreInteractions(dailyFratzeProvider, bikingPictureRepository);
+	
     }
 }
