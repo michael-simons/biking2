f56f5cda9187d84c59377a2167766b2d00fd8f47
Michael J. Simons <michael@simons.ac>
2014-08-26 20:40:25 +0200

    Added missing tests for series

--- src/test/java/ac/simons/biking2/highcharts/SeriesTest.java
+++ src/test/java/ac/simons/biking2/highcharts/SeriesTest.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
@@ -36,11 +37,22 @@ public class SeriesTest {
 		.withName("testName")
 		.withType("testType")
 		.withColor("AABBCC")
+		.withLineWidth(1.2)
+		.withFillOpacity(0.666)		
+		.withZIndex(1)
+		.linkTo("fump")
+		.marker()
+		    .build()
 		.build();
 	assertThat(series.getData(), is(equalTo(Arrays.asList(1.0, 2, 3))));
 	assertThat(series.getName(), is(equalTo("testName")));
 	assertThat(series.getType(), is(equalTo("testType")));
 	assertThat(series.getColor(), is(equalTo("AABBCC")));
+	assertThat(series.getLineWidth(), is(equalTo(1.2)));
+	assertThat(series.getFillOpacity(), is(equalTo(0.666)));
+	assertThat(series.getzIndex(), is(equalTo(1)));
+	assertThat(series.getLinkedTo(), is(equalTo("fump")));
+	assertThat(series.getMarker(), is(not(nullValue())));
 		
 	series = new Series.Builder<>(object -> object)
 		.withData(1, 2, 3)
@@ -51,5 +63,10 @@ public class SeriesTest {
 	assertThat(series.getName(), is(equalTo("testName")));
 	assertThat(series.getType(), is(equalTo("testType")));
 	assertThat(series.getColor(), is(nullValue()));
+	assertThat(series.getLineWidth(), is(nullValue()));
+	assertThat(series.getFillOpacity(), is(nullValue()));
+	assertThat(series.getzIndex(), is(nullValue()));
+	assertThat(series.getLinkedTo(), is(nullValue()));
+	assertThat(series.getMarker(), is(nullValue()));
     }
 }
