a80cac0fae5f5b6b85ff03474e8a11455dc7c3c6
Michael J. Simons <michael@simons.ac>
2014-09-11 21:45:00 +0200

    Also remove import

--- src/main/java/ac/simons/biking2/config/SecurityConfig.java
+++ src/main/java/ac/simons/biking2/config/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
