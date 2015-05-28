50e0eb4177b2d7a39483637f9212b031f208bd2b
Michael J. Simons <michael@simons.ac>
2015-05-28 11:47:34 +0200

    Remove explicit test properties location

--- src/test/java/ac/simons/biking2/config/TestConfig.java
+++ src/test/java/ac/simons/biking2/config/TestConfig.java
@@ -21,7 +21,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -29,7 +28,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * @author Michael J. Simons, 2014-02-16
  */
-@PropertySource("classpath:/application-test.properties")
 @Configuration
 @ComponentScan("ac.simons.biking2.config")
 @EnableAutoConfiguration
