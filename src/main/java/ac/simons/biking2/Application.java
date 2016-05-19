58b608d688ba9453a384b19cbba09776dc7a3adc
Michael J. Simons <michael@simons.ac>
2016-05-19 16:28:53 +0200

    Fix deprecations

--- src/main/java/ac/simons/biking2/Application.java
+++ src/main/java/ac/simons/biking2/Application.java
@@ -18,7 +18,7 @@ package ac.simons.biking2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 /**
