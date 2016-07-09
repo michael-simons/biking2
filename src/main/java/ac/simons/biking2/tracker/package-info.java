070c432d0cf7a47371aff6489e489ede722c37c3
Michael J. Simons <michael@simons.ac>
2016-07-09 21:26:18 +0200

    Add missing package-infos

---
+++ src/main/java/ac/simons/biking2/tracker/package-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2016 michael-simons.eu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * `tracker` provides the realtime interface for receiving the bikers current 
+ * location, the location repository and a REST interface for retrieving the
+ * locations of the last 30 minutes.
+ */
+package ac.simons.biking2.tracker;
