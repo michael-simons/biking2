1ffcb6d5668e26f2e38143d18a64a747d90fba2f
Michael J. Simons <michael@simons.ac>
2016-07-09 23:39:01 +0200

    Use consistent license header

--- src/main/java/ac/simons/biking2/bikingPictures/BikingPictureEntity.java
+++ src/main/java/ac/simons/biking2/bikingPictures/BikingPictureEntity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2016 Michael J. Simons.
+ * Copyright 2014-2016 michael-simons.eu.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
