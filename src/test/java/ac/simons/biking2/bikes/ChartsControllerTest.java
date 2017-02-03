fd40ed3f5661c5ca832a0305325bb2ec36d52f31
Michael J. Simons <michael@simons.ac>
2017-02-03 15:38:09 +0100

    Upgrade to Spring Boot 1.5.1

--- src/test/java/ac/simons/biking2/bikes/ChartsControllerTest.java
+++ src/test/java/ac/simons/biking2/bikes/ChartsControllerTest.java
@@ -36,7 +36,6 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.MessageSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
@@ -52,6 +51,7 @@ import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.iterableWithSize;
 import static org.mockito.Mockito.verify;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
