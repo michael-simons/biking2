bb576f47b05e31720905985e7167a700bfa6f713
Michael J. Simons <michael@simons.ac>
2017-11-29 21:57:39 +0100

    Boot2 - Fix changed type of connection-attribute in AutoConfigureTestDatabase

--- src/test/java/ac/simons/biking2/config/EndpointConfigIT.java
+++ src/test/java/ac/simons/biking2/config/EndpointConfigIT.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
