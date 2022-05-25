db232917bc27722982be5cd8c442d5f029a543e4
Michael J. Simons <michael@simons.ac>
2022-05-25 16:35:03 +0200

    Update to Spring Boot 2.7.

--- src/main/java/ac/simons/biking2/config/SecurityConfig.java
+++ src/main/java/ac/simons/biking2/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 michael-simons.eu.
+ * Copyright 2014-2022 michael-simons.eu.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,13 +24,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.to;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 /**
- * @author Michael J. Simons, 2014-02-19
+ * @author Michael J. Simons
+ * @since 2014-02-19
  */
 @Configuration(proxyBeanMethods = false)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -38,36 +39,32 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig {
 
     @Bean
-    WebSecurityConfigurerAdapter defaultWebSecurityConfigurerAdapter(@Value("${biking2.require-ssl:false}") final boolean requireSSL) {
-        return new WebSecurityConfigurerAdapter() {
-            @Override
-            protected void configure(final HttpSecurity http) throws Exception {
-                // @formatter:off
-                http
-                    .httpBasic()
-                        .and()
-                    .authorizeRequests()
-                        .antMatchers("/api/system/env/java.runtime.version")
-                            .permitAll()
-                        .requestMatchers(to(HealthEndpoint.class, InfoEndpoint.class, MetricsEndpoint.class))
-                            .permitAll()
-                        .requestMatchers(to(EnvironmentEndpoint.class))
-                            .authenticated()
-                        .antMatchers("/**").permitAll()
-                        .and()
-                    .sessionManagement()
-                        .sessionCreationPolicy(STATELESS)
-                        .and()
-                    .csrf()
-                        .disable()
-                    .headers()
-                        .frameOptions() // OEmbedController#embedTrack uses an iframe
-                        .disable();
-                // @formatter:on
-                if (requireSSL) {
-                    http.requiresChannel().anyRequest().requiresSecure();
-                }
-            }
-        };
+    SecurityFilterChain filterChain(@Value("${biking2.require-ssl:false}") final boolean requireSSL, final HttpSecurity http) throws Exception {
+        // @formatter:off
+        HttpSecurity builder = http
+            .httpBasic()
+                .and()
+            .authorizeRequests()
+                .antMatchers("/api/system/env/java.runtime.version")
+                    .permitAll()
+                .requestMatchers(to(HealthEndpoint.class, InfoEndpoint.class, MetricsEndpoint.class))
+                    .permitAll()
+                .requestMatchers(to(EnvironmentEndpoint.class))
+                    .authenticated()
+                .antMatchers("/**").permitAll()
+                .and()
+            .sessionManagement()
+                .sessionCreationPolicy(STATELESS)
+                .and()
+            .csrf()
+                .disable()
+            .headers()
+                .frameOptions() // OEmbedController#embedTrack uses an iframe
+                .disable().and();
+        // @formatter:on
+        if (requireSSL) {
+            builder = builder.requiresChannel().anyRequest().requiresSecure().and();
+        }
+        return builder.build();
     }
 }
